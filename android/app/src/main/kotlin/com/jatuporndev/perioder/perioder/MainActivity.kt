b2ab8f539c952369bd3ac589aca8a83e8e3aad0b
jatuporndev <jatuporn.dev@gmail.com>
2024-12-05 02:20:09 +0700

    init

---
+++ android/app/src/main/kotlin/com/jatuporndev/perioder/perioder/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.jatuporndev.perioder.perioder
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/screen/home.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class HomeView extends StatefulWidget {
+  const HomeView({super.key});
+
+  @override
+  State<HomeView> createState() => _HomeViewState();
+}
+
+class _HomeViewState extends State<HomeView> {
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Center(
+        child: Text("data"),
+      ),
+    );
+  }
+}
